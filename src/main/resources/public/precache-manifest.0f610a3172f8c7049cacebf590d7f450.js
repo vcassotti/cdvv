2be769c3a6a5480fbc470bcf411e4c94bf4ae0b7
Vincent Cassotti <vcassotti@gmail.com>
2018-06-29 00:31:03 +0200

    build front

---
+++ src/main/resources/public/precache-manifest.0f610a3172f8c7049cacebf590d7f450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77c34716eececde712e9",
+    "url": "/js/chunk-vendors.77c34716.js"
+  },
+  {
+    "revision": "2e6c5d40b2dccab827fb",
+    "url": "/js/app.2e6c5d40.js"
+  },
+  {
+    "revision": "8add8e0995d20331d4a99634ddeca36d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2e6c5d40b2dccab827fb",
+    "url": "/css/app.17f45697.css"
+  }
+];
